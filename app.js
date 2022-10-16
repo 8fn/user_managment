559f299bebee26abbf3ba098d03ee8164db51d4d
tsk <tanjilkh@gmail.com>
2022-10-16 13:32:35 +0100

    Setup middleware and layout

--- app.js
+++ app.js
@@ -8,4 +8,23 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Parse middleware
+app.use(express.urlencoded({extended: true})); 
+
+// Parse application/json
+app.use(express.json());
+
+// Static files
+app.use(express.static('public'));
+
+// Template engine
+const handlebars = exphbs.create({ extname: '.hbs',});
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+// Router
+app.get('', (req, res) => {
+    res.render('home');
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
