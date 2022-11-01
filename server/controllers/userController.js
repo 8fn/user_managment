9a7de58fe59ae2e1574331a03b29ad515c59e5b7
tsk <tanjilkh@gmail.com>
2022-11-01 12:59:21 +0000

    :zap: search query

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -27,9 +27,31 @@ exports.view = (req, res) => {
             } else {
                 console.log(err);
             }
-            console.log('The data from user table :\n', rows);
+           // console.log('The data from user table :\n', rows);
 
         });
     });
 }
 
+exports.find = (req, res) => {
+    pool.getConnection((err, connection) => {
+        if(err) throw err; // not connected!
+        console.log('Conected as ID ' + connection.threadId);
+
+        let searchTerm = req.body.search;
+
+
+        // User the connection 
+        connection.query('SELECT * FROM user WHERE first_name LIKE ? OR last_name LIKE ?', ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
+            // When done with connection, release it
+            connection.release();
+            if(!err){
+                res.render('home', {rows});
+            } else {
+                console.log(err);
+            }
+            console.log('The data from user table :\n', rows);
+
+        });
+    });
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,6 @@ const userController = require('../controllers/userController');
 
 // CRUD
 router.get('/', userController.view);
-
+router.post('/', userController.find);
 
 module.exports = router;
