664d79565f1ebd3d51bd61b3f4bf828b595aaa36
tsk <tanjilkh@gmail.com>
2022-11-01 14:09:40 +0000

    :zap: insert

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,5 +5,7 @@ const userController = require('../controllers/userController');
 // CRUD
 router.get('/', userController.view);
 router.post('/', userController.find);
+router.get('/adduser', userController.form);
+router.post('/adduser', userController.create);
 
 module.exports = router;
